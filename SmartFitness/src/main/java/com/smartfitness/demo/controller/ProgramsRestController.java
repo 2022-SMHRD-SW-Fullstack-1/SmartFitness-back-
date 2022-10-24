42095e9b76fac9e632e5cf52f3404b9a1e99ab13
smhrd <smhrd@DESKTOP-E836UEV>
2022-10-25 08:38:13 +0900

    25일 오전8시37분

--- SmartFitness/src/main/java/com/smartfitness/demo/controller/ProgramsRestController.java
+++ SmartFitness/src/main/java/com/smartfitness/demo/controller/ProgramsRestController.java
@@ -12,43 +12,63 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 import com.smartfitness.demo.model.Programs;
+import com.smartfitness.demo.model.TimetableAvailable;
+import com.smartfitness.demo.model.TimetableAvailable2;
 import com.smartfitness.demo.service.ProgramsService;
+import com.smartfitness.demo.service.TimetableAvailableService;
+import com.smartfitness.demo.service.TimetableAvailableService2;
 
 @RestController
 @RequestMapping("programs")
 public class ProgramsRestController {
-	
-	
+
 	Gson gson = new Gson();
-	
+
 	@Autowired
 	ProgramsService programsService;
-	
-	
-	//프로그램 정보 추가
+
+	@Autowired
+	TimetableAvailableService2 timetableAvailableService2;
+
+	// 프로그램 정보 추가
 	@PostMapping("programs/add")
 	public String programsAdd(@RequestBody Programs programs) {
 		System.out.println(programs.toString());
 		programsService.programsAdd(programs);
-	//~DB에 넣기 위해
-		
-		HashMap<String,Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("result", "success");
-		//~ 
-		String result = gson.toJson(resultMap);
-		//hashMap을 uri로 바꾸는 작업		
-		
-		return result;
-		
-	}
-	
-	//운동 프로그램 예약 가능 시간 확인
-	@GetMapping("/programs/timetable/{prSeq}")
-	public String selectTimetableAvailable2(@PathVariable("prSeq") int prSeq ) {
-		System.out.println(prSeq);
-	}
-	
-	
-	
+		// ~DB에 넣기 위해
 
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("result", "success");
+		// ~
+		String result = gson.toJson(resultMap);
+		// hashMap을 uri로 바꾸는 작업
+
+		return result;
+
+	}
+
+	// 운동 프로그램 예약 가능 시간 확인
+	@GetMapping("/programs/timetable/{prSeq}")
+	public String selectTimetableAvailable2(@PathVariable("prSeq") int prSeq,
+			@RequestBody TimetableAvailable2 timetableAvailable2) {
+		System.out.println(prSeq);
+		System.out.println(timetableAvailable2);
+		TimetableAvailable2 timetable = timetableAvailableService2.selectTimetableAvailable(prSeq);
+		String result = gson.toJson(timetable);
+		return result;
+	}
+
+	// 운동 프로그램 예약
+	@PostMapping("/programs/timetable/{prSeq}/reserv")
+	public String reservPrograms(@PathVariable("prSeq") int prSeq, @RequestBody String reserv) {
+		System.out.println(prSeq);
+		System.out.println(reserv);
+		int cnt = timetableAvailableService2.reservTimetable(prSeq, reserv);
+
+		if (cnt > 0) {
+			return "success";
+		} else {
+			return "fail";
+		}
+	}
 }
--- SmartFitness/src/main/java/com/smartfitness/demo/service/TimetableAvailableService2.java
+++ SmartFitness/src/main/java/com/smartfitness/demo/service/TimetableAvailableService2.java
@@ -1,5 +1,21 @@
 package com.smartfitness.demo.service;
 
-public class TimetableAvailableService2 {
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.smartfitness.demo.mapper.TimetableAvailableMapper2;
+import com.smartfitness.demo.model.TimetableAvailable2;
+
+public class TimetableAvailableService2 {
+	
+	@Autowired
+	TimetableAvailableMapper2 timetableAvailableMapper2;
+	
+	public TimetableAvailable2 selectTimetableAvailable(int prSeq) {
+		return timetableAvailableMapper2.selectTimetable(prSeq);
+	}
+	
+	public int reservTimetable(int prSeq, String reserv) {
+		int cnt = timetableAvailableMapper2.reservTimetable(prSeq,reserv);
+		return cnt;
+	}
 }
