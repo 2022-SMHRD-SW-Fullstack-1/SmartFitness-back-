2e0cf8a11d209c4591a022e23afaad75497e5742
smhrd <smhrd@DESKTOP-E836UEV>
2022-10-31 17:20:44 +0900

    월요일 5시20분

--- SmartFitness/src/main/java/com/smartfitness/demo/mapper/ProgramsMapper.java
+++ SmartFitness/src/main/java/com/smartfitness/demo/mapper/ProgramsMapper.java
@@ -1,13 +1,24 @@
 package com.smartfitness.demo.mapper;
 import java.util.HashMap;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import com.smartfitness.demo.model.CurrentPrograms;
 import com.smartfitness.demo.model.Programs;
 @Mapper
 public interface ProgramsMapper {
 	
-	public int addPg(Programs program);
 	
-	public HashMap<String,Object> selectCurrPg(int month);
+	public int addPg(Programs programs);
+	
+	public List<HashMap> selectCurrPg(int month);
+	
+	public int reservPg(HashMap<String,Object> map);
+	
+	public int reservPg2(HashMap<String,Object> map);
+
+	public int cancelPg(int num);
+	
+	
 	
 }
--- SmartFitness/src/main/java/com/smartfitness/demo/service/ProgramsService.java
+++ SmartFitness/src/main/java/com/smartfitness/demo/service/ProgramsService.java
@@ -1,5 +1,6 @@
 package com.smartfitness.demo.service;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,8 +15,18 @@ public class ProgramsService {
 	public int addPg(Programs programs) {
 		return programsMapper.addPg(programs);
 	}
-	public HashMap<String,Object> selectCurrPg(int month) {
+	public List<HashMap> selectCurrPg(int month) {
 		return programsMapper.selectCurrPg(month);
 	}
+	public int reservPg(HashMap<String, Object> map) {
+		//current_programs에 인원 +
+		programsMapper.reservPg2(map);
+		return programsMapper.reservPg(map);
+	}
+	public int cancelPg(int num) {
+		return programsMapper.cancelPg(num);
+	}
+	
+	
 	
 }
