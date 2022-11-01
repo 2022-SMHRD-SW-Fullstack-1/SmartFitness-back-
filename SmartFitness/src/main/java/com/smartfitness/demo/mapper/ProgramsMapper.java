f20e6d43c5520e5b47b1a60c63c86ed3770807ce
smhrd <smhrd@DESKTOP-E836UEV>
2022-11-01 14:14:07 +0900

    화요일 14시14분

--- SmartFitness/src/main/java/com/smartfitness/demo/mapper/ProgramsMapper.java
+++ SmartFitness/src/main/java/com/smartfitness/demo/mapper/ProgramsMapper.java
@@ -3,8 +3,11 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+
+import com.google.gson.JsonElement;
 import com.smartfitness.demo.model.CurrentPrograms;
 import com.smartfitness.demo.model.Programs;
+import com.smartfitness.demo.model.Trainer;
 @Mapper
 public interface ProgramsMapper {
 	
@@ -18,6 +21,17 @@ public interface ProgramsMapper {
 	public int reservPg2(HashMap<String,Object> map);
 
 	public int cancelPg(int num);
+	
+	//트레이너 정보 확인
+	public HashMap<String,Object> confirmT(int num);
+
+	public List<HashMap> confirmAllT(int ex);
+
+	public int addTrainer(Trainer trainer);
+
+	public int openPg(CurrentPrograms curr);
+
+	public int rate(HashMap<String, Object> map);
 	
 	
 	
