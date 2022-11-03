b94440aeb9a7c5602b861ed3b4531fe746bdcd48
Min-SungJo <123or@naver.com>
2022-11-03 17:27:42 +0900

    성조 11.03 17:25 security-role

--- SmartFitness/src/main/java/com/smartfitness/demo/mapper/MembersMapper.java
+++ SmartFitness/src/main/java/com/smartfitness/demo/mapper/MembersMapper.java
@@ -8,12 +8,21 @@ import com.smartfitness.demo.model.MembersDetail;
 @Mapper
 public interface MembersMapper {
 
+	//회원 정보 인증,인가
 	MembersDetail findByUserId(String mem_id);
+	Members read(String mem_id);
 
+	//회원가입
 	void join(Members members);
 	
+	//로그인
 	public Members login(Members members);
 
+	//회원정보 수정
 	void update(Members members);
+
+	//회원탈퇴
+	void delete(String mem_id);
+
 
 }
