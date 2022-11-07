7918914e0645f8a9f463b0ff2653610be046eea8
Min-SungJo <123or@naver.com>
2022-11-07 18:06:49 +0900

    auth보완

--- SmartFitness/src/main/java/com/smartfitness/demo/config/auth/Auth.java
+++ SmartFitness/src/main/java/com/smartfitness/demo/config/auth/Auth.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Auth {
 	private String accessToken;
-	private String refreshToken;
+//	private String refreshToken;
 	private String mem_id;
 	private String mem_auth;
 //	private String ctr_seq;
