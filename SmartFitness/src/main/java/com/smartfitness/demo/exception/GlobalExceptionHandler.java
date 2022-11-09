fdb95776eaf1342d6275edd9f9c890f63e656552
Min-SungJo <123or@naver.com>
2022-11-09 14:02:22 +0900

    iamport exception

--- SmartFitness/src/main/java/com/smartfitness/demo/exception/GlobalExceptionHandler.java
+++ SmartFitness/src/main/java/com/smartfitness/demo/exception/GlobalExceptionHandler.java
@@ -1,45 +1,45 @@
-//package com.smartfitness.demo.exception;
-//
-//import java.net.BindException;
-//
-//import javax.servlet.http.HttpServletRequest;
-//
-//import org.hibernate.exception.ConstraintViolationException;
-//import org.springframework.dao.DataIntegrityViolationException;
-//import org.springframework.dao.DuplicateKeyException;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.MethodArgumentNotValidException;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//import org.springframework.web.servlet.NoHandlerFoundException;
-//
-//import lombok.extern.slf4j.Slf4j;
-///**
-// * exception발생 시 전역으로 처리할 exception handler
-// * **/
-//@RestControllerAdvice // 모든 restcontrolelr에서 발생하는 exception을 처리한다.
-//public class GlobalExceptionHandler extends Exception{
-//
-//	@ExceptionHandler(value= {CustomException.class})
-//	protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e){
-//		return ErrorResponse.toResponseEntity(e.getErrorCode());
-//	}
-//
-//	@ExceptionHandler(value= {ConstraintViolationException.class, DataIntegrityViolationException.class})
-//	protected ResponseEntity<ErrorResponse> handleDuplicationException(){
-//		return ErrorResponse.toResponseEntity(ErrorCode.INTER_SERVER_ERROR);
-//	}	
-//	
-//	@ExceptionHandler(NoHandlerFoundException.class)
-//	protected ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException e,
-//			HttpServletRequest request) {
-//		return ErrorResponse.toResponseEntity(ErrorCode.NOT_FOUND);
-//	}
-//	
-//	@ExceptionHandler(Exception.class)
-//	protected ResponseEntity<ErrorResponse> handleException(Exception e, HttpServletRequest request) {
-//		return ErrorResponse.toResponseEntity(ErrorCode.BAD_REQUEST);
-//	}
-//    
-//}
+package com.smartfitness.demo.exception;
+
+import java.net.BindException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import lombok.extern.slf4j.Slf4j;
+/**
+ * exception발생 시 전역으로 처리할 exception handler
+ * **/
+@RestControllerAdvice // 모든 restcontrolelr에서 발생하는 exception을 처리한다.
+public class GlobalExceptionHandler extends Exception{
+
+	@ExceptionHandler(value= {CustomException.class})
+	protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e){
+		return ErrorResponse.toResponseEntity(e.getErrorCode());
+	}
+
+	@ExceptionHandler(value= {ConstraintViolationException.class, DataIntegrityViolationException.class})
+	protected ResponseEntity<ErrorResponse> handleDuplicationException(){
+		return ErrorResponse.toResponseEntity(ErrorCode.INTER_SERVER_ERROR);
+	}	
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	protected ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException e,
+			HttpServletRequest request) {
+		return ErrorResponse.toResponseEntity(ErrorCode.NOT_FOUND);
+	}
+	
+	@ExceptionHandler(Exception.class)
+	protected ResponseEntity<ErrorResponse> handleException(Exception e, HttpServletRequest request) {
+		return ErrorResponse.toResponseEntity(ErrorCode.BAD_REQUEST);
+	}
+    
+}
--- SmartFitness/src/main/java/com/smartfitness/demo/payment/PaymentsController.java
+++ SmartFitness/src/main/java/com/smartfitness/demo/payment/PaymentsController.java
@@ -25,7 +25,7 @@ public class PaymentsController {
 		// 처음에 요청했던 금액 저장하기
 		try {
 			paymentsService.insertPaymentInfo(paymentsModel);
-			return "success";	
+			return "ok";	
 		}catch(Exception e){
 			return "ng";
 		}
@@ -35,18 +35,19 @@ public class PaymentsController {
 	@PostMapping("/complete")
 	public String complete(@RequestBody PaymentsModel paymentsModel) {
 		// STEP5. 결제 정보 검증 및 저장하기
-		try {
+
 			// STEP5-2. 결제 정보 조회하기
 			// 액세스 토큰(access token) 발급 받기
 			String accessToken = paymentsService.getAccessToken(paymentsModel); 
+			System.out.println(accessToken);
 			// imp_uid로 아임포트 서버에서 결제 정보 조회
 			PaymentsModel paymentInfo = paymentsService.getPaymentInfo(accessToken, paymentsModel);
 			// STEP5-3. 결제 정보 검증 후 저장하기
 			// DB에서 결제되어야 하는 금액 조회
-			String amountToBePaid = paymentsService.getAmountToBePaid(paymentsModel);
+			int amountToBePaid = paymentsService.getAmountToBePaid(paymentsModel);
 			int amount = paymentInfo.getAmount();
 			// 결제 검증하기
-			if(Integer.parseInt(amountToBePaid) == amount) {
+			if(amountToBePaid == amount) {
 				paymentsService.updatePaymentInfo(paymentInfo); // DB에 결제 정보 저장
 				if(paymentInfo.getStatus().equals("paid")) { // 결제 완료
 					return "success";
@@ -58,8 +59,6 @@ public class PaymentsController {
 			}else { // 결제금액 불일치. 위/변조 된 결제
 				return "forgery";
 			}
-		}catch(Exception e) {
-			return "exception";
-		}
+
 	}
 }
