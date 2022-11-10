0c68c09624ef28420f237e7dd694eee58cf805db
smhrd <smhrd@DESKTOP-E836UEV>
2022-11-10 16:31:07 +0900

    1631 기간 연장 완료

--- SmartFitness/src/main/java/com/smartfitness/demo/service/MembersService.java
+++ SmartFitness/src/main/java/com/smartfitness/demo/service/MembersService.java
@@ -21,83 +21,81 @@ import com.smartfitness.demo.model.MembersDetail;
 
 @Service
 public class MembersService {
-	
+
 	@Autowired
 	MembersMapper membersMapper;
 	@Autowired
 	PasswordEncoder passwordEncoder;
 	@Autowired
 	JwtTokenProvider jwtTokenProvider;
-	
-	//회원가입
-	public void join(Members members) throws Exception{
+
+	// 회원가입
+	public void join(Members members) throws Exception {
 		String mem_id = members.getMem_id();
 		String DBmem_id = membersMapper.read(mem_id).getMem_id();
-		if(DBmem_id!=null) {
-			//아이디 중복일 때, 예외 처리
-			throw new CustomException(ErrorCode.MEM_CONFLICT);	
+		if (DBmem_id != null) {
+			// 아이디 중복일 때, 예외 처리
+			throw new CustomException(ErrorCode.MEM_CONFLICT);
 		}
 		int cnt = membersMapper.join(members);
-		if(cnt==0) {
-			//회원가입 실패
+		if (cnt == 0) {
+			// 회원가입 실패
 			throw new CustomException(ErrorCode.MEM_BAD_REQUEST);
 		}
 	}
-	
-	//로그인
-	public Auth login(Members members) throws Exception{
+
+	// 로그인
+	public Auth login(Members members) throws Exception {
 		String mem_id = members.getMem_id();
 		String mem_pw = members.getMem_pw();
 		Map<String, Object> result = new HashMap<>();
 		MembersDetail md = membersMapper.findByUserId(mem_id);
-		if(md==null) {
-			//아이디를 찾을 수 없음
+		if (md == null) {
+			// 아이디를 찾을 수 없음
 			throw new CustomException(ErrorCode.MEMBER_NOT_FOUND);
-		}
-		else if(!passwordEncoder.matches(mem_pw, md.getPassword())) {
-			//잘못된 비밀번호
+		} else if (!passwordEncoder.matches(mem_pw, md.getPassword())) {
+			// 잘못된 비밀번호
 			throw new CustomException(ErrorCode.WRONG_PW);
-		}
-		else {
+		} else {
 			String DBmem_id = md.getMem_id();
 			String DBmem_auth = md.getMem_auth();
-			String DBmem_email =md.getMem_auth();
+			String DBmem_email = md.getMem_auth();
 			String DBmem_name = md.getMem_name();
-			String DBmem_phone =md.getMem_phone();
-			String accessToken= jwtTokenProvider.createToken(md, "Access-Token");
-			Auth auth = new Auth(accessToken,DBmem_id,DBmem_auth,DBmem_email,DBmem_name,DBmem_phone);
+			String DBmem_phone = md.getMem_phone();
+			String accessToken = jwtTokenProvider.createToken(md, "Access-Token");
+			Auth auth = new Auth(accessToken, DBmem_id, DBmem_auth, DBmem_email, DBmem_name, DBmem_phone);
 			return auth;
 		}
 	}
-	//회원정보 수정
-	public void update(Members members) throws Exception{
-		String rawPassword=members.getMem_pw();
-		String encPassword=passwordEncoder.encode(rawPassword);
+
+	// 회원정보 수정
+	public void update(Members members) throws Exception {
+		String rawPassword = members.getMem_pw();
+		String encPassword = passwordEncoder.encode(rawPassword);
 		members.setMem_pw(encPassword);
 		int result = membersMapper.update(members);
-		if(result==0) {
-			//아이디 중복
+		if (result == 0) {
+			// 아이디 중복
 			throw new CustomException(ErrorCode.MEM_CONFLICT);
 		}
 	}
 
-	//회원탈퇴
+	// 회원탈퇴
 	public void delete(String mem_id, String mem_pw) throws Exception {
 		MembersDetail md = membersMapper.findByUserId(mem_id);
-		if(!passwordEncoder.matches(mem_pw, md.getPassword())) {
-			//잘못된 비밀번호
+		if (!passwordEncoder.matches(mem_pw, md.getPassword())) {
+			// 잘못된 비밀번호
 			throw new CustomException(ErrorCode.WRONG_PW);
-		}
-		else {
-		int result = membersMapper.delete(mem_id);
-			if(result==0) {
-				//회원탈퇴 실패
+		} else {
+			int result = membersMapper.delete(mem_id);
+			if (result == 0) {
+				// 회원탈퇴 실패
 				throw new CustomException(ErrorCode.MEM_BAD_REQUEST);
 			}
 		}
 	}
 
-	//멤버십 결제
+	// 멤버십 결제
 	public void insertInfo(Map<String, Object> paymentsModel) {
 		System.out.println(paymentsModel);
 		Map<String,Object> map = new HashMap<String,Object>();
@@ -116,54 +114,28 @@ public class MembersService {
 		//마감기한
 		map.put("amount", amount);
 		
+		System.out.println("a:"+id);
+		int num = membersMapper.readM(id);
+		
+		if(num>0) {
+			if(amount==100) {
+			membersMapper.updateInfo(map);
+			}else if(amount==500) {
+			membersMapper.updateInfo2(map);
+			}
+		}else {
 		
 		if(amount==100) {
 		membersMapper.insertInfo(map);
-		}
-		if(amount==500) {
+		}else if(amount==500) {
 		membersMapper.insertInfo2(map);
+			}
 		}
 	}
 }
-
-
-
-
-
-
 
 ////현재 날짜 불러오기(그냥 DB에서 하겠음)
 //Date date = new Date();
 //SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 //String date2 = formatter.format(date);
 //map.put("date",date2);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- SmartFitness/src/main/java/ex/JwtTokenProvider2.java
+++ SmartFitness/src/main/java/ex/JwtTokenProvider2.java
@@ -1,10 +1,17 @@
 //package ex;
 //
+//import java.util.Base64;
+//import java.util.Date;
+//
 //import javax.annotation.PostConstruct;
 //
 //import org.springframework.security.core.userdetails.UserDetailsService;
 //import org.springframework.stereotype.Component;
 //
+//import com.smartfitness.demo.model.MembersDetail;
+//
+//import io.jsonwebtoken.Claims;
+//import io.jsonwebtoken.Jwts;
 //import lombok.RequiredArgsConstructor;
 //
 //@RequiredArgsConstructor
@@ -12,6 +19,7 @@
 //public class JwtTokenProvider2 {
 //
 //		private String secretKey = "webfirewood";
+//		//secretKey는 토큰 유효성 확인시 사용하기에 외부에 노출되지 않게 해야한다.
 //		
 //		//토큰 유효시간 지정 30분
 //		private long tokenValidTime = 30*60*1000L;
@@ -19,8 +27,48 @@
 //		
 //		//객체 초기화, secret키를 Base64로 encoding한다.
 //		@PostConstruct
-//		protected void init
+//		protected void init(){
+//			secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+//		}
 //		
-//		
+//		//JWT 토큰 생성,Token은 String으로 구성되기 때문에 반환 타입이 String이다.
+//		public String createToken(MembersDetail Mem, String tokenName) {
+//			Date now = new Date();
+//			Claims claims = Jwts.claims().setSubject(Mem.getMem_id()); //JWT payload에 저장되는 정보단위
+//			//claim은 pay
+//			claims.put("token", tokenName); 
+//			
+//		}
 //		
 //}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
